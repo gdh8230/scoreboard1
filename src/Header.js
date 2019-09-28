6805d54e50d2ff758bfefe07d35d972ae0c750ec
MK <gdh8230@naver.com>
2019-09-28 17:45:08 +0900

    stats 추가

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import {Stats} from "./Stats";
+
 
 export const Header = (props) => {
 	console.log(props);
 	return (
 		<header className="header">
-			<h1 className="h1">ScoreBoard</h1>
-			<span className="stats">Players: {props.totalPlayers}</span>
+			<Stats/>
+			<h1>{props.title}</h1>
 		</header>
 	);
 };
