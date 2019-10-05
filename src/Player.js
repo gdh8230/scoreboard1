ff8f64865a05f7b659cd1c5cd86ed87774eebfb5
김아롱 <grownupwendy@naver.com>
2019-10-05 13:09:26 +0900

    shouldComponentUpdate

--- src/Player.js
+++ src/Player.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Counter} from "./Counter";
 
-export class Player extends React.PureComponent {
+export class Player extends React.Component {
 	render() {
 		console.log(this.props.name, ' rendered');
 		
@@ -16,5 +16,10 @@ export class Player extends React.PureComponent {
 				<Counter score={this.props.score} changeScore={this.props.changeScore} id={this.props.id}/>
 			</div>
 		);
+	}
+
+	shouldComponentUpdate(nextProps, nextState, nextContext) {
+		console.log(nextProps);
+		return nextProps.score !== this.props.score ? true : false
 	}
 }
